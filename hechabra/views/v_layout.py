f3ca36a3058466fb99a98b1ad2ed0813fa7559ed
Ubuntu <ubuntu@ip-172-31-42-214.us-west-2.compute.internal>
2015-08-28 04:25:40 +0000

    Major updates to make links functional

---
+++ hechabra/views/v_layout.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.insert(0, '/var/www/hechabra')
+
+from hechabra import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('home.html', name='Home')
+
+@app.route('/create/word')
+def create_word():
+    return render_template('create_word.html', name='Create Word')
+
+@app.route('/create/definition')
+def create_definition():
+    return render_template('create_definition.html', name='Create Definition')
+
+@app.route('/vote')
+def vote():
+    return render_template('vote.html', name='Vote')
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html', name='Stats')
